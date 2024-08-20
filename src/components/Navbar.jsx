d899280dc43c7f1e670b8c250c6ecd68391ae7e0
Alex Xu <xualex008@gmail.com>
2024-08-19 21:52:23 -0400

    rally interest form

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,8 @@ export default function TransparentNavbar({type}) {
             <div className="dropdown forms-dropdown">
               <div className={linkType}>FORMS AND PAYMENTS<span className={arrowType}><svg width="9" height="9" viewBox="0 0 15 15"><path d="M2.1,3.2l5.4,5.4l5.4-5.4L15,4.3l-7.5,7.5L0,4.3L2.1,3.2z"></path></svg></span></div>
               <div className="dropdown-content forms-content">
-                <a href="/forms/membership">Membership Form</a>                
+                <a href="/forms/membership">Membership Form</a>     
+                <a href="/forms/rally-interest">Rally Interest Form</a>           
               </div>
             </div>
 
@@ -97,7 +98,8 @@ export default function TransparentNavbar({type}) {
           </div>
           <div className="menu-link" onClick={() => {toggleSelection(1)}}>FORMS AND PAYMENTS<span className="menu-down-arrow"><svg width="14" height="14" viewBox="0 0 15 15"><path d="M2.1,3.2l5.4,5.4l5.4-5.4L15,4.3l-7.5,7.5L0,4.3L2.1,3.2z"></path></svg></span></div>
           <div className="menu-link-dropdown" style={{display: (selectedType == 1) ? "flex" : "none"}}>
-            <a href="/forms/membership">Membership Form</a>            
+            <a href="/forms/membership">Membership Form</a>  
+            <a href="/forms/rally-interest">Rally Interest Form</a>                     
           </div>
           <div className="menu-link" onClick={() => {toggleSelection(2)}}>EVENTS<span className="menu-down-arrow"><svg width="14" height="14" viewBox="0 0 15 15"><path d="M2.1,3.2l5.4,5.4l5.4-5.4L15,4.3l-7.5,7.5L0,4.3L2.1,3.2z"></path></svg></span></div>
           <div className="menu-link-dropdown" style={{display: (selectedType == 2) ? "flex" : "none"}}>
