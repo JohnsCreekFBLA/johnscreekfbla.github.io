0e75fd5dc2694dd1d70c16130b77129ad3d8756a
Alex Xu <xualex008@gmail.com>
2025-09-04 08:08:00 -0400

    text fix

--- src/components/events/meeting.jsx
+++ src/components/events/meeting.jsx
@@ -10,7 +10,7 @@ const Meetings = ({ meetings }) => {
             link: "/meetings/FBLA_Aug_Meeting.pdf"
         },
         flc: {
-            title: "FLC Interest Meeting Slidess",
+            title: "FLC Interest Meeting Slides",
             about: "FLC Interest Meeting",
             date: "September 2, 2025",
             link: "/meetings/FBLA_FLC_Interest_Meeting.pdf"
